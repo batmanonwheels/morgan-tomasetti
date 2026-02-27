3bc282d2f6a8a0aa5121f679795442081dd1f1e7
Devin Garcia <devingarcia5423@gmail.com>
2026-02-27 17:16:34 -0500

    Begin work on nav color effect

--- components/header.tsx
+++ components/header.tsx
@@ -1,27 +1,60 @@
+"use client";
+
+import Link from "next/link";
+
 export const Header = () => {
-	const directory: string[][] = [
-		['BIO', '/bio'],
-		['COMMERCIAL', '/commercial'],
-		['THEATRE', '/theatre'],
-		['TV/FILM', '/screen'],
-		['MUSIC', '/music'],
-		['DIGIS', '/digis'],
-		['HEADSHOTS', '/headshots'],
-		['GALLERY', '/gallery'],
-		['CONTACT/RESUME', '/contact'],
-	];
-	return (
-		<header className='fixed bottom-0 flex gap-10 p-8'>
-			<h1 className='items-center text-2xl'>MORGAN TOMASETTI</h1>
-			<nav className='flex items-center gap-5'>
-				{directory.map(([title, link], i) => {
-					return (
-						<a className='opacity-65' href={link} key={title + i}>
-							{title}
-						</a>
-					);
-				})}
-			</nav>
-		</header>
-	);
+  const directory: string[][] = [
+    ["BIO", "/bio", "#fec5bb"],
+    ["COMMERCIAL", "/commercial", "#fcd5ce"],
+    ["THEATRE", "/theatre", "#fae1dd"],
+    ["TV/FILM", "/screen", "#f8edeb"],
+    ["MUSIC", "/music", "#e8e8e4"],
+    ["DIGIS", "/digis", "#d8e2dc"],
+    ["HEADSHOTS", "/headshots", "#ece4db"],
+    ["GALLERY", "/gallery", "#ffe5d9"],
+    ["CONTACT/RESUME", "/contact", "#ffd7ba"],
+  ];
+
+  const changeHeaderBg = (color: string) => {
+    const header = document.querySelector("#header");
+    if (header) {
+      header.classList.remove(header.classList[7]);
+      header.classList.add(`bg-[${color}]`);
+      console.log(header);
+    }
+  };
+
+  const removeHeaderBg = () => {
+    const header = document.querySelector("#header");
+    if (header) {
+      header.classList.remove(header.classList[7]);
+      console.log(header);
+    }
+  };
+
+  return (
+    <header
+      className="fixed bottom-0 flex gap-10 p-8 w-full border-t bg-blue-500"
+      id="header"
+    >
+      <Link href="/">
+        <h1 className="items-center text-3xl">MORGAN TOMASETTI</h1>
+      </Link>
+      <nav className="flex items-center gap-5">
+        {directory.map(([title, link, color], i) => {
+          return (
+            <Link
+              className={``}
+              href={link}
+              key={title + i}
+              onMouseOver={() => changeHeaderBg(color)}
+              onMouseLeave={() => removeHeaderBg()}
+            >
+              {title}
+            </Link>
+          );
+        })}
+      </nav>
+    </header>
+  );
 };
