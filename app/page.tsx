bd24119c6adaf8f3f28109f6d49858603ba55ed8
Devin Garcia <devingarcia5423@gmail.com>
2026-03-03 13:05:56 -0500

    move header to layout.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,7 @@
-import { Header } from '@/components/header';
-
 export default function Home() {
-	return (
-		<div className='min-h-screen font-sans bg-zinc-50 dark:bg-black'>
-			<Header />
-			<main className='flex items-center justify-center '></main>
-		</div>
-	);
+  return (
+    <div className="min-h-screen font-sans bg-zinc-50 dark:bg-black">
+      <main className="flex items-center justify-center "></main>
+    </div>
+  );
 }
