4330f51e60803b3d2b76aa50ff390a981168cab9
Devin Garcia <devingarcia5423@gmail.com>
2026-03-03 14:32:13 -0500

    Add image to homepage, and add cloudinary hostname to img/next.config

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,19 @@
+import Image from "next/image";
+
 export default function Home() {
   return (
     <div className="min-h-screen font-sans bg-zinc-50 dark:bg-black">
-      <main className="flex items-center justify-center "></main>
+      <main className="flex items-center justify-center">
+        <Image
+          alt={"Heashot of Morgan Tomasetti"}
+          src={
+            "https://res.cloudinary.com/dmmn0gqaf/image/upload/v1772565495/morgan-tomasetti/headshots/MT-233_qwymaw.jpg"
+          }
+          width={1920}
+          height={1080}
+          className="w-full h-[calc(100dvh-6rem)] object-cover object-top"
+        />
+      </main>
     </div>
   );
 }
--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,11 @@ const nextConfig: NextConfig = {
   experimental: {
     viewTransition: true,
   },
+  images: {
+    remotePatterns: [
+      new URL("https://res.cloudinary.com/dmmn0gqaf/image/upload/**"),
+    ],
+  },
 };
 
 export default nextConfig;
