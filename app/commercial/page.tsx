b8fa11df95510ee88b275c5dea47e0a3bc8dcdc8
Devin Garcia <devingarcia5423@gmail.com>
2026-03-03 13:05:23 -0500

    set up routes

---
+++ app/commercial/page.tsx
@@ -0,0 +1,7 @@
+export default function Commercial() {
+  return (
+    <div className="min-h-screen font-sans bg-zinc-50 dark:bg-black">
+      <main className="flex items-center justify-center "></main>
+    </div>
+  );
+}
